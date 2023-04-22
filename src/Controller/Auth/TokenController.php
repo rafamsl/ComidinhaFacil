78301cbda5cd72730c78dcb6fa4c4aae442dc116
rafamsl <rafael.msl81@gmail.com>
2023-04-22 11:04:42 +0200

    Add POST Auth Login

---
+++ src/Controller/Auth/TokenController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller\Auth;
+
+use App\Entity\ApiToken;
+use App\Entity\User;
+use App\Repository\ApiTokenRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TokenController extends AbstractController
+{
+    #[Route('/api/get_token', name:'app_get_token', methods: ['POST'])]
+    public function getToken(ApiTokenRepository $apiTokenRepository): object
+    {
+        $user = $this->getUser();
+
+        if(!$user instanceof User){
+            return $this->json([
+                'error' => 'User not found'
+            ]);
+        }
+
+        $apiToken = new ApiToken($user);
+        $apiTokenRepository->save($apiToken, true);
+
+        return $this->json([
+            'user.id' => $user->getId(),
+            'apiToken' => $apiToken->getToken(),
+            'expiresAt' => $apiToken->getExpiresAt()
+        ]);
+    }
+
+}
--- src/Entity/ApiToken.php
+++ src/Entity/ApiToken.php
@@ -2,21 +2,42 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
 use App\Repository\ApiTokenRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: ApiTokenRepository::class)]
+#[ApiResource(
+    operations:[
+        new Post()
+    ],
+    normalizationContext:[
+        'groups' => ['apiToken:read']
+    ],
+    denormalizationContext: [
+    'groups' => ['apiToken:write']
+],
+)]
 class ApiToken
 {
+    public function __construct(User $user){
+        $this->token = bin2hex(random_bytes(60));
+        $this->owner = $user;
+        $this->expiresAt = new \DateTimeImmutable('+1 hour');
+    }
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['apiToken:read'])]
     private ?string $token = null;
 
     #[ORM\Column]
+    #[Groups(['apiToken:read'])]
     private ?\DateTimeImmutable $expiresAt = null;
 
     #[ORM\ManyToOne(inversedBy: 'apiTokens')]
@@ -33,34 +54,13 @@ class ApiToken
         return $this->token;
     }
 
-    public function setToken(string $token): self
-    {
-        $this->token = $token;
-
-        return $this;
-    }
-
     public function getExpiresAt(): ?\DateTimeImmutable
     {
         return $this->expiresAt;
     }
 
-    public function setExpiresAt(\DateTimeImmutable $expiresAt): self
-    {
-        $this->expiresAt = $expiresAt;
-
-        return $this;
-    }
-
     public function getOwner(): ?User
     {
         return $this->owner;
-    }
-
-    public function setOwner(?User $owner): self
-    {
-        $this->owner = $owner;
-
-        return $this;
     }
 }
